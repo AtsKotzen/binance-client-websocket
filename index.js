2105983e1592483c8766cf6db2bb5994e51b5530
AtsKotzen <atsaudio@gmail.com>
2022-04-23 19:30:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const readlineSync = require('readline-sync');
+const symbol = readlineSync.question('Qual o par de moedas vamos monitorar?');
+
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/bookTicker');
+
+ws.onopen = () => {
+    ws.send(JSON.stringify({
+        "method": "SUBSCRIBE",
+        "params": [
+            `${symbol}@bookTicker`
+        ],
+        "id": 1
+    }))
+}
+
+ws.onmessage = (event) => {
+    // Limpar console antes da resposta
+    process.stdout.write('\033c');
+    const obj = JSON.parse(event.data);
+    //Mostrando dados
+    console.log(obj)
+    // // mostrando symbol = s
+    // console.log(`Symbol: ${obj.s}`);
+    // // lower ask = a
+    // console.log(`Best Ask: ${obj.a}`);
+    // // lower bid = b
+    // console.log(`Best Bid: ${obj.b}`);
+    
+}
+
+
